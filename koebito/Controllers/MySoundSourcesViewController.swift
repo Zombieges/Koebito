9e13a9ac026d8c3633df07114f97ed0add0b27d2
legobrokkori <ralirulego103@gmail.com>
2017-08-12 21:23:52 +0900

    TableView を MySoundSourcesView に定義し直した

--- koebito/Controllers/MySoundSourcesViewController.swift
+++ koebito/Controllers/MySoundSourcesViewController.swift
@@ -11,20 +11,35 @@ import UIKit
 class MySoundSourcesViewController: UIViewController {
 
     @IBOutlet weak var profileImage: UIView!
-    @IBOutlet weak var soundsList: UIView!
+    @IBOutlet weak var soundsListTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewDidLayoutSubviews() {
         
         // set up profileImageView
         let ProfileImageViewNib = ProfileImageView.instantiate(ownerOrNil: nil, options: nil)
         profileImage.addSubview(ProfileImageViewNib)
         // set up tableView
-        let SoundsListTableViewNib = SoundsList.instantiate(ownerOrNil: nil, options: nil)
-        soundsList.addSubview(SoundsListTableViewNib)
+        let nib = UINib(nibName: "SoundsLiatTableViewCell", bundle: nil)
+        soundsListTableView.register(nib, forCellReuseIdentifier: "SoundsListCellIdentifier")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
     }
 }
 
+
+extension MySoundSourcesViewController: UITableViewDataSource,
+                                        UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SoundsListCellIdentifier") as! SoundsLiatTableViewCell
+        cell.textLabel?.text = ""
+        return cell
+    }
+}
---
+++ koebito/View/MySoundSourcesView/SoundsLiatTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SoundsLiatTableViewCell.swift
+//  koebito
+//
+//  Created by Kazuhiro Watanabe on 2017/08/12.
+//
+//
+
+import UIKit
+
+class SoundsLiatTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var soundsList: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
